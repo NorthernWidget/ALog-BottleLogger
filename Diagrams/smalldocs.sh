0a38f4b2b3e8aa3d7d3d1ebb185a7a1a230d182c
Andy Wickert <andy@northernwidget.com>
2017-05-11 02:27:53 +0200

    Larger PNGs: sufficient resolution

--- Diagrams/smalldocs.sh
+++ Diagrams/smalldocs.sh
@@ -3,5 +3,5 @@
 for infileBasename in ALogComponents ALogUsersLayout
 do
 	gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/printer -dNOPAUSE -dBATCH  -dQUIET -sOutputFile=${infileBasename}_smallfile.pdf $infileBasename.pdf
-	convert ${infileBasename}_smallfile.pdf -compress Zip -background white -flatten ${infileBasename}.png
+	convert -units PixelsPerInch -density 300 -resize 50% -background white -flatten ${infileBasename}.pdf ${infileBasename}.png
 done
